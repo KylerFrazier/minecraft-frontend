45c66af80c37d7a0baa7a4369f05124baf480681
Ethan Yeh <ethan.yc.yeh@berkeley.edu>
2022-01-02 19:47:14 -0800

    add routes to skeleton pages

---
+++ src/login.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  Link
+} from "react-router-dom";
+import { Header } from './header'
+
+export const Login: React.FC = () => {
+  return (
+    <>
+      <Header/>
+      <div>Here's the login page</div>
+    </>
+  )
+}
